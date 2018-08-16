96f823be1b9f7f57a08168d0216be259265ca59f
jackz <zk416604134@163.com>
2018-08-17 07:35:28 +0800

    thymeleaf 1

--- src/main/java/com/zk/myspittr/web/WebConfig.java
+++ src/main/java/com/zk/myspittr/web/WebConfig.java
@@ -1,5 +1,7 @@
 package com.zk.myspittr.web;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,11 +14,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.zk.myspittr.web")
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 
 //    /**
 //     * 配置视图解析器
@@ -55,24 +68,62 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         return messageSource;
     }
 
-    /**
-     * 配置使用Apache Tiles需要的bean
-     * @return TilesConfigurer
-     */
+//    /**
+//     * 配置使用Apache Tiles需要的bean
+//     * @return TilesConfigurer
+//     */
+//    @Bean
+//    public TilesConfigurer tilesConfigurer() {TilesConfigurer tilesConfigurer = new TilesConfigurer();
+//        tilesConfigurer.setDefinitions("/WEB-INF/layout/tiles.xml","/WEB-INF/views/**/tiles.xml");
+//        tilesConfigurer.setCheckRefresh(true);
+//        return tilesConfigurer;
+//    }
+//
+//    /**
+//     * 配置使用Apach tiles需要的视图解析器
+//     * @return ViewResolver
+//     */
+//    @Bean
+//    public ViewResolver viewResolver() {
+//        return new TilesViewResolver();
+//    }
+
+
     @Bean
-    public TilesConfigurer tilesConfigurer() {TilesConfigurer tilesConfigurer = new TilesConfigurer();
-        tilesConfigurer.setDefinitions("/WEB-INF/layout/tiles.xml","/WEB-INF/views/**/tiles.xml");
-        tilesConfigurer.setCheckRefresh(true);
-        return tilesConfigurer;
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setCacheable(true);
+        return  templateResolver;
     }
 
-    /**
-     * 配置使用Apach tiles需要的视图解析器
-     * @return ViewResolver
-     */
     @Bean
-    public ViewResolver viewResolver() {
-        return new TilesViewResolver();
+    public SpringTemplateEngine templateEngine(){
+        // SpringTemplateEngine automatically applies SpringStandardDialect and
+        // enables Spring's own MessageSource message resolution mechanisms.
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
+        // speed up execution in most scenarios, but might be incompatible
+        // with specific cases when expressions in one template are reused
+        // across different data types, so this flag is "false" by default
+        // for safer backwards compatibility.
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
     }
 
+    @Bean
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
+        // NOTE 'order' and 'viewNames' are optional
+//        viewResolver.setOrder(1);
+//        viewResolver.setViewNames(new String[] {".html", ".xhtml"});
+        return viewResolver;
+    }
 }
