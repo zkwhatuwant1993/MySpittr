c33d6a994f3b5197b2f1de4d9ed667a6a414687e
Jack Zyy <zkwhatuwant@gmail.com>
2018-08-21 18:14:14 +0800

    web.xml配置

--- src/main/java/com/zk/myspittr/config/SpittrWebAppInitializer.java
+++ src/main/java/com/zk/myspittr/config/SpittrWebAppInitializer.java
@@ -4,17 +4,18 @@ import com.zk.myspittr.web.WebConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 
-public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[] {RootConfig.class};
-    }
-
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {WebConfig.class};
-    }
-
-    protected String[] getServletMappings() {
-        //将一个或者多个url路径mapping到DispatcherServlet
-        return new String[] {"/"};
-    }
+public class SpittrWebAppInitializer  {
+//    extends AbstractAnnotationConfigDispatcherServletInitializer
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class[] {RootConfig.class};
+//    }
+//
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class[] {WebConfig.class};
+//    }
+//
+//    protected String[] getServletMappings() {
+//        //将一个或者多个url路径mapping到DispatcherServlet
+//        return new String[] {"/"};
+//    }
 }
